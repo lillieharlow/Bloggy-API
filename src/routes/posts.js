340c3309d761556185faaa5a5d17735c390d443e
Lillie <chapman.lillie@gmail.com>
2025-11-29 13:46:46 +1100

    feat: defined CRUD for blog posts - src/routes/posts.

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -18,6 +18,7 @@
 const express = require('express');
 const auth = require('../middleware/jwtAuth');
 const Post = require('../models/Post');
+
 const router = express.Router();
 
 // ========== GET /api/v1/posts — List all posts (Public) ==========
@@ -61,12 +62,75 @@ router.get('/:postId', async (request, response, next) => {
 router.use(auth);
 
 // ========== POST /api/v1/posts — Create new post (Auth required) ==========
-// router.post('/', async (request, response, next) => {});
+router.post('/', async (request, response, next) => {
+  try {
+    const post = await Post.create({
+      title: request.body.title,
+      body: request.body.body,
+      image: request.body.image,
+      tags: request.body.tags,
+      author: request.user.id,
+    })
+    response.status(201).json({
+      success: true,
+      data: post,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 // ========== PATCH /api/v1/posts/:postId — Update existing post (Auth required) ==========
-// router.patch('/:postId', async (request, response, next) => {});
+router.patch('/:postId', async (request, response, next) => {
+  try {
+    const post = await Post.findOneAndUpdate(
+      { _id: request.params.postId, author: request.user.id },
+      {
+        $set: {
+          title: request.body.title,
+          body: request.body.body,
+          image: request.body.image,
+          tags: request.body.tags,
+        }
+      },
+      { new: true, runValidators: true } // Re-validates on update
+    );
+
+    if (!post) {
+      const error = new Error('Post not found / not authroized to make changes.');
+      error.status = 404;
+      return next(error);
+    }
+
+    response.status(200).json({
+      success: true,
+      data: post
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 // ========== DELETE /api/v1/posts/:postId — Delete post (Auth required) ==========
-// router.delete('/:postId', async (request, response, next) => {});
+router.delete('/:postId', async (request, response, next) => {
+  try {
+    const post = await Post.findOneAndDelete({
+      _id: request.params.postId,
+      author: request.user.id
+    });
+    if (!post) {
+      const error = new Error('Post not found / not authorized to delete.');
+      error.status = 404;
+      return next(error);
+    }
+
+    response.status(200).json({
+      success: true,
+      message: 'Post deleted successfully!'
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
